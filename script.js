c9feabfb51d421c942ec608e156a94ba3d4dd6cd
Max Edmonds <maxedmonds05@gmail.com>
2020-06-13 19:46:34 -0700

    add UVindex response

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-let city, url, APIkey, etc;
+let city, url, APIkey = "";
+var citiesDiv = document.querySelector("searched_cities_container")
 
-var submitButton = document.querySelector("#submitButton")
+var searchbtn = document.querySelector("#searchbtn")
+//start with empty array
+let cities = [];
 
 url = "https://api.openweathermap.org/data/2.5/forecast?q=";
 currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
 APIkey = "&APPID=187b175e5dec11ce8a3bab842028437f";
 
 
-
-/*submitButton.addEventListener("click", function () {*/
+// searchbtn.addEventListener("click", function(event) {
 city = $("#cityInput").val()
 $.ajax({
-    url: currenturl + "new%20york" + APIkey,
+    url: currenturl + "portland" + APIkey,
     method: "GET"
 }).then(function (response) {
     console.log(response)
     $.ajax({
         url: `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&exclude=minutely,hourly${APIkey}`,
         method: "GET"
-    }).then(function (res) {
-        console.log(res)
+    }).then(function (UVresponse) {
+        console.log(UVresponse)
         let temp = Math.round(((response.main.temp - 273.15) * 9 / 5 + 32))
-
-        // $("#today_temp").html("Temperature: " + res.current.temp)
         $("#today_temp").html("Temperature: " + temp + String.fromCharCode(176) + "F");
+        $("#today_humidity").html("Humidity: " + response.main.humidity + '%');
+        $("#today_wind_speed").html("Wind Speed: " + response.wind.speed + ' ' + 'MPH');
+        $("#today_UV_Index").html("UV Index: " + UVresponse.current.uvi);
+
 
     })
 })
-//})
+// })
